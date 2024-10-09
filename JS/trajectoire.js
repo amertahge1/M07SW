abd2cdaf32f5c837c84ae3ff9d05f492188353d1
Amel <amel210201@gmail.com>
2024-10-09 15:42:52 +0200

    tracer la ligne

--- JS/trajectoire.js
+++ JS/trajectoire.js
@@ -1,6 +1,6 @@
 
 document.getElementById("nav_trajectoire").addEventListener('click',trajectoire);
-
+var isStartingPosition=false;
 
 
 function trajectoire()
@@ -28,41 +28,75 @@ function trajectoire()
 function afficherplan()
 { 
     
+    canvas = document.getElementById('canvas_trajectoire');
+    ctx = canvas.getContext('2d');
+    console.log(ctx);
+
+    var img = new Image();
+    img.src = './plan.png';
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0); 
+    } 
     
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           //document.getElementById("section").innerHTML=this.responseText;
-            let pagehtml=document.getElementById('section');
-            pagehtml.innerHTML=this.response;
 
+    document.getElementById("btn_tracer").addEventListener('click',tracer);
    
-            canvas = document.getElementById('canvas_trajectoire');
-            ctx = canvas.getContext('2d');
-            console.log(ctx);
-           
-            var img = new Image();
-            img.src = './plan.png';
-            img.onload = function () {
-                ctx.drawImage(img, 0, 0);  
-                ctx.beginPath(); // Start a new path
-                ctx.moveTo(30, 50); // Move the pen to (30, 50)
-                ctx.lineTo(150, 100); // Draw a line to (150, 100)
-                ctx.stroke(); // Render the path
-                ctx.moveTo(50, 30); // Move the pen to (30, 50)
-        ctx.lineTo(100, 150); // Draw a line to (150, 100)
-        ctx.stroke(); // Render the path
-        }
+    document.getElementById("btn_entregistrer").addEventListener('click',enregistrer);
+    document.getElementById("btn_effacer").addEventListener('click',effacer);
 
-        
-        document.getElementById("nav_creer").addEventListener('click',creer);
-        document.getElementById("nav_charger").addEventListener('click',charger);
-     }
-    }
-xhttp.open("GET", "trajectoire.html", true);
-xhttp.send();
+    document.getElementById("nav_creer").addEventListener('click',creer);
+    document.getElementById("nav_charger").addEventListener('click',charger);
+
 }
 
+function tracer () {
+
+    var x=parseInt(document.getElementById("posx").value); 
+    var y=parseInt(document.getElementById("posy").value);
+    
+
+    console.log("ok");
+    if(isStartingPosition==false){
+        ctx.beginPath(); // Start a new path
+        ctx.moveTo(x, y);
+        ctx.moveTo(x-5,y-5);
+        ctx.lineTo( x+5, y+5);
+        ctx.lineTo( x+5, y+5);
+        ctx.lineTo( x+5,y+5);
+        ctx.moveTo(x+5, y-5);
+        ctx.lineTo(x-5,y+5);
+        ctx.moveTo(x, y);
+        ctx.stroke();
+        afficher.innerHTML = `start ${x} ${y}<br>command <br>takeoff`;
+        isStartingPosition=true;
+    }else{
+        ctx.lineTo(x, y);
+        ctx.lineTo(x+5,y+5);
+        ctx.lineTo(x+5,y+5);
+        ctx.moveTo(x+5,y-5);
+        ctx.lineTo(x-5,y+5);
+        ctx.stroke();
+        afficher.innerHTML += `<br>go ${x} ${y} 0 50`;
+    }
+}
+
+function enregistrer(){
+    console.log("ok");
+}
+function effacer(){
+    //console.log("ok");
+    canvas = document.getElementById('canvas_trajectoire');
+    ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var img = new Image();
+    img.src = './plan.png';
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0); 
+    } 
+
+
+    
+}
 function creer()
 {
 
